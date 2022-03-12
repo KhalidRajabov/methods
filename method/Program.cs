6e10f4ae6f5524e2ee76c9467c83e9344f56a551
dbBurzum <khalidsr@code.edu.az>
2022-03-12 15:54:11 +0400

    Add project files.

---
+++ method/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region  method
+            //Console.WriteLine("Hello World!");
+            //int result = Sum(5, 6);
+            //Console.WriteLine(result);
+            //string person = Info("Xalid", "Recebov", 27);
+            //Console.WriteLine(person);
+            //Print("salam");
+            //int divide = Divide(6, 3)
+            //Console.WriteLine(divide);
+            //Multiple(14, 5);
+
+            //int[] array = { 1, 2, 3, 4,};
+            /*  int result = SubMassiveElement(1,2,3,4,5);
+              Console.WriteLine(result);*/
+
+            //Console.WriteLine(Subtract(5));
+           // Console.WriteLine(Sum("asfasda"));
+            #endregion
+        }
+
+        #region method
+        /* static int Sum(int num1 , int num2)
+         {
+             return num1 + num2;
+         }
+         static string Info(string ad, string soyad, int age)
+         {
+             return $"{ad} {soyad} {age}";
+         }
+         static void Print(string a)
+         {
+             Console.WriteLine($"{a}");
+         }
+         static int Divide(int num1, int num2)
+         {
+             if(num1 > num2)
+             {
+                 return num1;
+             }
+             else
+             {
+                 return num2;
+             }
+         }
+         static void Multiple(int num1, int num2)
+         {
+             if (num1 > 10)
+             {
+                 Console.WriteLine($"{num1}");
+                 return;
+             }
+             else
+             {
+                 Console.WriteLine(num2);
+             }
+         }*/
+        /*static int SubMassiveElement(params int[] arr)
+        {
+            int sum = 0;
+            foreach (var item in arr)
+            {
+                sum += item;
+            }
+            return sum;
+        }*/
+
+        /*        static int  Subtract(int num1, int num2=2)
+                {
+                    return num1 - num2;
+                }*/
+
+/*        static int Sum(int num1)
+        {
+            return num1;
+        }
+        static string Sum(string word)
+        {
+            return word;
+        }
+        static int Sum( params int[] array )
+        {
+            return 0;
+        }*/
+        #endregion
+
+    }
+}
